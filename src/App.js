1e136901b9002d3057eab9cf977beb9c0ec6c4fa
eagraf <graf.ethan@gmail.com>
2019-05-04 17:18:06 -0400

    Starter code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -6,8 +6,22 @@ function App() {
     return (
         <div className='container'>
             <h2>Inventory Starter Project</h2>
+            <Inventory />
         </div>
     );
+}
+
+class Inventory extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return <h4>Inventory</h4>;
+    }
 }
 
 export default App;

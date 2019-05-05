1adef82d6fee254dc82c60e0c07cae68b785fc86
eagraf <graf.ethan@gmail.com>
2019-05-05 14:24:28 -0400

    Added comments and TODOs

--- src/App.js
+++ src/App.js
@@ -11,17 +11,28 @@ function App() {
     );
 }
 
+// Starter code for the inventory component.
 class Inventory extends Component {
     constructor(props) {
         super(props);
+        // TODO setup initial state for the component
     }
 
+    // componentDidMount is the best place in the component lifecycle to get data
+    // from the API when the component first loads.
+    // Check out: https://reactjs.org/docs/state-and-lifecycle.html
     componentDidMount() {
+        // TODO GET request to Postman mock server
     }
 
+    // Renders the component into the DOM
+    // Check out: https://reactjs.org/docs/rendering-elements.html
     render() {
         return <h4>Inventory</h4>;
+        // TODO render a list of components representing items in the inventory
     }
 }
+
+// TODO implement an Item component that represents the view for a single item.
 
 export default App;
